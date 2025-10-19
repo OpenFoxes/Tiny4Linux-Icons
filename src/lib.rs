da89434af0d1de33b6cc8a35d6b8c4afed58abc2
Bono Fox <git@bono-fox.de>
2025-10-19 18:01:28 +0200

    ✨ Call for ready to use handle and path
    - function to get absolute path of asset
    - function to get a ready to use iced-image-handle

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
-// Cargo generates a library containing all the assets.
+use iced::widget::image::Handle;
+use once_cell::sync::Lazy;
+use std::env;
+use std::path::{Path, PathBuf};
+
+static ROOT: Lazy<PathBuf> = Lazy::new(|| {
+    // path root should be the crate root
+    let root = env!("CARGO_MANIFEST_DIR");
+    Path::new(root).to_path_buf()
+});
+
+pub fn absolute_path_for_t4l_asset<P: AsRef<Path>>(relative_path: P) -> PathBuf {
+    ROOT.join(relative_path)
+}
+
+pub fn handle_t4l_asset<P: AsRef<Path>>(relative_path: P) -> Handle {
+    let abs_path = absolute_path_for_t4l_asset(relative_path);
+    Handle::from_path(abs_path)
+}
